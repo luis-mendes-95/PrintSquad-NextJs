aaf1607cbac9f739cb2b36dd51bd69ded564df1f
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-07-17 13:51:45 -0300

    fix first impressions when open the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import { GetServerSideProps, NextPage } from "next";
 import CardServiceOrder from "../components/card";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import {DivHomeBase} from "../styles/home"
+import { DivHomeBase } from "../styles/home";
 import { useAuth } from "@/contexts/authContext";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { parseCookies } from "nookies";
 
 interface HomeProps {
   serviceOrders: serviceOrderData[];
@@ -17,26 +18,24 @@ const Home: NextPage<HomeProps> = ({ serviceOrders }) => {
 
   const router = useRouter();
 
-  const { user, checkLoggedIn } = useAuth()
+  const { user, checkLoggedIn } = useAuth();
 
   useEffect(() => {
+
     checkLoggedIn()
-    
-    if (!user) {
-      router.push("/about");
-    }
-  }, [user, router]);
 
-
+  }, [])
+  
 
   return (
     <DivHomeBase>
       <Header />
       <main>
-        {
-          user &&
-            <ul className="serviceOrderCards">
-            {serviceOrders.length === 0 && <div className="divNoOrders">Você não possui nenhuma ordem de serviço</div>}
+        {user ? (
+          <ul className="serviceOrderCards">
+            {serviceOrders.length === 0 && (
+              <div className="divNoOrders">Você não possui nenhuma ordem de serviço</div>
+            )}
             {serviceOrders.map((individualServiceOrder) => {
               return (
                 <li key={individualServiceOrder.id}>
@@ -45,34 +44,36 @@ const Home: NextPage<HomeProps> = ({ serviceOrders }) => {
               );
             })}
           </ul>
-        }
-
-        {
-          !user &&
-        <section className="sectionHomePageVisitor">
-          sectionHomePageVisitor
-        </section>
-        }
-
+        ) : (
+          <section className="sectionHomePageVisitor" style={{backgroundColor:"black", height:"100vh"}}>sectionHomePageVisitor</section>
+        )}
       </main>
       <Footer />
-
     </DivHomeBase>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (cxt) => {
-  const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImF2YWlsdG9uamVzdXM1QGhvdG1haWwuY29tIiwiaWF0IjoxNjg5Mzg3NzU5LCJleHAiOjE2ODkzOTEzNTksInN1YiI6IjJjOWQxNjQ1LWEyY2MtNDc0Ny1iMzQ5LTQ4ZDcyNDkxMDM5YyJ9.QjXdKNeVhh45dFrmYfNnvyJlAO_BOw-deHE0SRUEkig";
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { "printsquad.token": token } = parseCookies(context);
 
-  const response = await api.get<serviceOrderData[]>("/serviceOrders", {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  let serviceOrders: serviceOrderData[] = [];
+
+  if (token) {
+    try {
+      const response = await api.get<serviceOrderData[]>("/serviceOrders", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      serviceOrders = response.data;
+    } catch (error) {
+      console.error("Erro ao obter as ordens de serviço:", error);
+      // Aqui você pode adicionar o tratamento de erro adequado, como redirecionar para uma página de erro ou exibir uma mensagem para o usuário.
+    }
+  }
 
   return {
-    props: { serviceOrders: response.data },
+    props: { serviceOrders },
   };
 };
 
